a99c33691aacabe3f3ba37b0114ee26fbe928aab
Keshav Sharma <keshavsharma@Keshavs-MacBook-Air.local>
2026-07-08 12:53:42 +0530

    db url set

--- hr_portal/hr_portal/settings.py
+++ hr_portal/hr_portal/settings.py
@@ -31,7 +31,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
-    default='.onrender.com,localhost,127.0.0.1',
+    default='hr-portal-backend-wej7.onrender.com,localhost,127.0.0.1',
     cast=Csv()
 )
 
